3bbc25fe94714125825d7cc9ed00dd4d27865733
dylan <7257923+dmnuggins@users.noreply.github.com>
2023-07-20 21:10:44 -0700

    User interface

---
+++ index.js
@@ -0,0 +1,24 @@
+// evalute the result given the operator and inputs
+function operate(operator, prev, input) {
+  switch (operator) {
+    case '/':
+      // divide
+      break
+    case 'x':
+      // multiply
+      break
+    case '-':
+      // subtract
+      break
+    case '+':
+      // add
+      break
+    default:
+      break
+  }
+}
+
+// modify the current input
+function modify(input) {}
+
+// add click handler to all buttons
