328eaee0544e8749a2e1912c7cb2a41589f64994
dylan <7257923+dmnuggins@users.noreply.github.com>
2023-07-20 22:46:57 -0700

    Number & clearing buttons now do something

--- index.js
+++ index.js
@@ -1,6 +1,38 @@
-const prev = 0
-const input = 0
-const operator = ''
+let prevInput = 0
+let curInput = 0
+let operator = ''
+
+// add click handler to all buttons
+const numButtons = document.querySelectorAll('button')
+const subDisplay = document.querySelector('.sub')
+const mainDisplay = document.querySelector('.main')
+
+// add click listener to all number
+numButtons.forEach((button) => {
+  if (button.className === 'num') {
+    button.addEventListener('click', () => {
+      const numInput = parseInt(button.textContent)
+      console.log(button.id, ' clicked')
+      curInput = curInput * 10 + numInput
+      mainDisplay.textContent = formatNumberWithCommas(curInput)
+    })
+  }
+})
+
+const clearEntryButton = document.querySelector('#ce')
+const clearButton = document.querySelector('#c')
+
+clearEntryButton.addEventListener('click', () => {
+  curInput = 0
+  mainDisplay.textContent = curInput.toString()
+})
+
+clearButton.addEventListener('click', () => {
+  prevInput = 0
+  curInput = 0
+  mainDisplay.textContent = curInput.toString()
+  subDisplay.textContent = ''
+})
 
 // evalute the result given the operator and inputs
 function operate(operator, prev, input) {
@@ -25,4 +57,7 @@ function operate(operator, prev, input) {
 // modify the current input
 function modify(input) {}
 
-// add click handler to all buttons
+// format numeric value with commas
+function formatNumberWithCommas(number) {
+  return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+}
