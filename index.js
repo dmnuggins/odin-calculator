473ef6e443f51d0b79b63e93b60de1dee413450b
dylan <7257923+dmnuggins@users.noreply.github.com>
2023-07-26 02:27:54 -0700

    Refactored operation and evaluation functions

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 let prevInput = null
-let curInput = null
-let operator = ''
-
-// state
-let opPressed = false
+let curInput = ''
+let curOperator = null
+let needDisplayReset = false
 
 // add click handler to all buttons
 const buttons = document.querySelectorAll('button')
+const operators = document.querySelectorAll('.operator')
+// console.log(operators)
 const subDisplay = document.querySelector('.sub')
-const mainDisplay = document.querySelector('.main')
+const mainDisplay = document.querySelector('.main') // should limit to 20 chars max
 const clearEntryButton = document.querySelector('#ce')
 const clearButton = document.querySelector('#c')
 
@@ -16,95 +16,118 @@ const clearButton = document.querySelector('#c')
 buttons.forEach((button) => {
   if (button.className === 'num') {
     button.addEventListener('click', () => {
-      const numInput = parseInt(button.textContent)
-      // console.log(button.innerText, ' clicked')
-      if (opPressed) {
-        opPressed = false
-        curInput = 0
-      }
-      curInput = curInput * 10 + numInput
-      console.log('curInput: ', curInput)
-      mainDisplay.textContent = formatNumberWithCommas(curInput)
+      handleNumInput(button.textContent)
     })
   }
   // QUEUES OPERATOR
   if (button.className === 'operator') {
     button.addEventListener('click', () => {
-      // When operator is click
-      handleMod(button.innerText)
+      handleOperatorInput(button.textContent)
     })
   }
   // MODIFIES CUR INPUT
   if (button.className === 'mod') {
     button.addEventListener('click', () => {
-      console.log(button.innerText, ' mod clicked')
+      console.log(button.textContent, ' mod clicked')
       // handle modifier button input
     })
   }
   // EQUALS
   if (button.id === 'eval') {
     button.addEventListener('click', () => {
-      // console.log(button.innerText, ' clicked')
-      handleEval()
+      handleEvalInput()
     })
   }
 })
 
-clearEntryButton.addEventListener('click', () => {
-  curInput = 0
-  mainDisplay.textContent = curInput.toString()
-})
+// clearEntryButton.addEventListener('click', () => {
+//   curInput = null
+//   initialState = true
+//   mainDisplay.textContent = curInput.toString()
+// })
 
-clearButton.addEventListener('click', () => {
-  prevInput = 0
-  curInput = 0
-  mainDisplay.textContent = curInput.toString()
-  subDisplay.textContent = ''
-})
+// clearButton.addEventListener('click', () => {
+//   prevInput = null
+//   curInput = null
+//   mainDisplay.textContent = 0
+//   subDisplay.textContent = ''
+//   initialState = true
+//   curOperator = null
+//   newInput = true
+//   lockFirstOperand = false
+//   evaluated = false
+// })
 
-function handleMod(btnText) {
-  operator = btnText
-  opPressed = true
-  if (prevInput && curInput) {
-    console.log('ready to eval')
-  } else if (!prevInput && curInput) {
-    prevInput = curInput
-    curInput = null
-  }
-  subDisplay.textContent = (!prevInput ? ' ' : `${prevInput}`) + ` ${operator}`
+const resetScreen = () => {
+  mainDisplay.textContent = ''
+  needDisplayReset = false
 }
 
-function handleEval() {
-  answer = operate(operator, prevInput, curInput)
-  mainDisplay.textContent = operate(operator, prevInput, curInput)
-  subDisplay.textContent = `${prevInput} ${operator} ${curInput} =`
-  prevInput = answer
+function handleNumInput(num) {
+  // clear display value to prep for updated num input
+  // when: first input of calc, after eval & after operator
+  if (mainDisplay.textContent === '0' || needDisplayReset) {
+    resetScreen()
+  }
+  // append display number
+  mainDisplay.textContent += num
+  // set current input to main display num
+}
+// should set up prevInput
+function handleOperatorInput(operator) {
+  if (curOperator !== null) handleEvalInput()
+
+  prevInput = mainDisplay.textContent
+  curOperator = operator
+  subDisplay.textContent = `${prevInput} ${curOperator}`
+  console.log('prevInput: ', prevInput)
+  console.log('curOperator: ', curOperator)
+  needDisplayReset = true
+}
+
+// need conditional to ensure there are 2 operands before handleEval
+function handleEvalInput() {
+  if (curOperator === null || needDisplayReset) return
+
+  if (curOperator === '÷' && mainDisplay.textContent === '0') {
+    mainDisplay.textContent = 'Result is undefined'
+    return
+  }
+  console.log('curInput: ', curInput)
+  curInput = mainDisplay.textContent
+  mainDisplay.textContent = operate(
+    curOperator,
+    parseFloat(prevInput),
+    parseFloat(curInput)
+  )
+  subDisplay.textContent = `${prevInput} ${curOperator} ${curInput} =`
+  curOperator = null
 }
 
 // evalute the result given the operator and inputs
-function operate(operator, prev, input) {
-  let ans = 0
+function operate(operator, a, b) {
   switch (operator) {
     case '÷':
       // divide
-      ans = div(prev, input)
+      if (b === 0) return null
+      else return div(a, b)
+
       break
     case '×':
       // multiply
-      ans = mult(prev, input)
+      return mult(a, b)
       break
     case '-':
       // subtract
-      ans = sub(prev, input)
+      return sub(a, b)
       break
     case '+':
       // add
-      ans = add(prev, input)
+      return add(a, b)
       break
     default:
       break
   }
-  return ans
 }
 
 function add(a, b) {
